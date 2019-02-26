667de878730a8d8ab54dfbcbf045e2f819b45470
surnamej <32624724+surnamej@users.noreply.github.com>
2019-02-26 18:34:06 +0700

    Update lab13_2.cpp

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,39 @@ int main(){
 }
 
 // Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+void inputMatrix(double A[][N]){
+	for(int i=0; i<N; i++){
+		cout<<"Row "<<i+1<<": ";
+		for(int j=0; j<N; j++){
+			cin>>A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i = 0;i < N;i++){
+		for(int j = 0;j < N;j++){
+			if(i == 0 ||j == 0||i == N-1||j == N-1){
+				B[i][j] = false;
+			}else if(A[i][j] >= A[i+1][j] && A[i][j] >= A[i-1][j] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i][j-1]){
+				B[i][j] = true;
+			}else{
+				B[i][j] = false;
+			}
+		}
+		
+	}
+}
+
+void showMatrix(const bool B[][N]){
+	for(int i = 0;i < N;i++){
+		for(int j = 0;j < N;j++){
+			if(B[i][j] == true){
+				cout<<" "<<"1"<<" ";
+			}else{
+				cout<<" "<<"0"<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
