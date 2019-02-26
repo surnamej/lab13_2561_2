152bbda3a00e6b212bdf617911563496322c3760
surnamej <32624724+surnamej@users.noreply.github.com>
2019-02-26 18:32:34 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,10 +11,31 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
+	for(int s=1; s<N; s++){
+		int idx;
+		for (int i = 0;i < N;i++){
+			if (i==s) cout<<"["<<d[i]<<"]";
+			else cout<<" "<<d[i]<<" ";
+		} 
+		cout << "=>";
+		for(int i=s; i>0; i--){
+			if(d[i] > d[i-1]) {
+				idx=i-1;
+				swap(d,i,i-1);
+			}else{
+				idx=i;
+				break;
+			}
+		}
+		for (int j = 0;j < N;j++){
+			if (j==idx) cout<<"["<<d[j]<<"]";
+			else cout<<" "<<d[j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
 //Write your code here.
 
-}
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
